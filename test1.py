09bed538743f3e493a74b403ef97f16cfb3609d4
Brandon Russell <rbrandon87@gmail.com>
2019-01-19 18:52:30 -0500

    Start

---
+++ test1.py
@@ -0,0 +1,11 @@
+age = input("What is your age?")
+name = input("What is your name?")
+AgeCompare = int(age)
+
+print("Hello " + name + " you are " + age)
+
+while AgeCompare >= 1:
+    print(str(AgeCompare))
+    AgeCompare -= 1
+
+print("Done")
